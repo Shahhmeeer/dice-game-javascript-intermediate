f9cdabd50adbcfba787bf765706bf6bb2ec334c9
Shahmeer <shahmeerasim1999@gmail.com>
2023-03-30 18:26:34 +0500

    added flag to winners

--- main.js
+++ main.js
@@ -18,19 +18,19 @@ const randomNumberForPlayer1 = Math.floor(Math.random() * 6);
 const randomNumberForPlayer2 = Math.floor(Math.random() * 6);
 // console.log(randomNumberForPlayer1);
 // console.log(randomNumberForPlayer2);
-
 // console.log(diceImageSourceLinks[randomNumberForPlayer1]);
 
+// Changing the Images Source.
 diceOne.setAttribute('src', `images/${diceImageSourceLinks[randomNumberForPlayer1]}`);
 diceTwo.setAttribute('src', `images/${diceImageSourceLinks[randomNumberForPlayer2]}`);
-
+// Changing the text to show who won.
 if (randomNumberForPlayer1 === randomNumberForPlayer2) {
   firstPlayerP.innerHTML = 'It is a Draw';
   secondPlayerP.innerHTML = 'It is a Draw';
 } else if (randomNumberForPlayer1 > randomNumberForPlayer2) {
   console.log('player 1 wins');
-  firstPlayerP.innerHTML = `${firstPlayerPValue} wins!`;
+  firstPlayerP.innerHTML = `🚩${firstPlayerPValue} wins!`;
 } else {
   console.log('player 2 wins');
-  secondPlayerP.innerHTML = `${secondPlayerPValue} wins!`;
+  secondPlayerP.innerHTML = `${secondPlayerPValue} wins🚩`;
 }
